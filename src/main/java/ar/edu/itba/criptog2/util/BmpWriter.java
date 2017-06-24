2a558799b02bb9d9f45d8cab2d66f296633cea4d
Juan Li Puma <juan.lipuma@restorando.com>
2017-06-24 18:56:52 -0300

    Improve BmpWriter
    - Fix write method
    - Manually write all data
    - Auto-calculate some fields

--- src/main/java/ar/edu/itba/criptog2/util/BmpWriter.java
+++ src/main/java/ar/edu/itba/criptog2/util/BmpWriter.java
@@ -1,11 +1,10 @@
 package ar.edu.itba.criptog2.util;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.file.Files;
-import java.util.Arrays;
 
 /**
  * Simple BMP writer. Initialized with basic BMP data, and can write out BMP to file.
@@ -13,7 +12,6 @@ import java.util.Arrays;
 public class BmpWriter {
 
     private File file;
-    private byte[] fileData;
     private String id;
     private int fileSize;
     byte[] reservedBytes;
@@ -32,99 +30,53 @@ public class BmpWriter {
     private byte[] extraHeaderBytes;
     private byte[] extraPictureBytes = new byte[0];
 
-    public BmpWriter(String bitmapImagePath) throws IOException {
-        if(bitmapImagePath == null || bitmapImagePath.isEmpty()) {
-            throw new IllegalArgumentException("Invalid bitmap image path provided");
-        }
-        file = new File(bitmapImagePath);
-        if (!file.exists()) {
-            throw new IllegalArgumentException("Image does not exist");
-        }
-        if (file.isDirectory()) {
-            throw new IllegalArgumentException("Provided path is a directory, not a file");
-        }
-        if (!file.canRead()) {
-            throw new IllegalArgumentException("Image is not readable");
-        }
-        parse();
-    }
-
     private BmpWriter(BmpWriterBuilder builder) {
-        this.file = builder.file;
-        this.fileData = builder.fileData;
+        this.file = builder.outputFile;
         this.id = builder.id;
-        this.fileSize = builder.fileSize;
+        this.fileSize = 14 + 40 + extraPictureBytes.length + builder.pictureData.length;
         this.reservedBytes = builder.reservedBytes;
-        this.pictureOffset = builder.pictureOffset;
-        this.infoHeaderLength = builder.infoHeaderLength;
+        this.pictureOffset = 14 + 40 + extraPictureBytes.length;
+        this.infoHeaderLength = 40;
         this.width = builder.width;
         this.height = builder.height;
         this.numPlanes = builder.numPlanes;
         this.bitsPerPixel = builder.bitsPerPixel;
         this.compressionType = builder.compressionType;
-        this.pictureSize = builder.pictureSize;
+        this.pictureSize = builder.pictureData.length;
         this.horizontalResolution = builder.horizontalResolution;
         this.verticalResolution = builder.verticalResolution;
         this.numUsedColors = builder.numUsedColors;
         this.numImportantColors = builder.numImportantColors;
         this.pictureData = builder.pictureData;
         this.extraHeaderBytes = builder.extraHeaderBytes;
-        this.extraPictureBytes = builder.extraPictureBytes;
+        this.extraPictureBytes = new byte[0];
     }
 
     /**
-     * Parses the BMP this parser was initialized with, reading all header data and image data.
+     * Writes the BMP to the file this writer was initialized with.
      *
-     * @throws IOException If an I/O error occurs reading any of the file.
-     * @see <a href="https://stackoverflow.com/a/10497780/2333689">Source for converting File to byte[]</a>
-     */
-    private void parse() throws IOException {
-        fileData = Files.readAllBytes(file.toPath());
-//        ByteArrayInputStream stream = new ByteArrayInputStream(fileData);
-//        ArrayList<Byte> buffer = new ArrayList<>();
-        id = new String(Arrays.copyOfRange(fileData, 0, 2));
-        fileSize = bytesToInt(Arrays.copyOfRange(fileData, 2, 6));
-        reservedBytes = Arrays.copyOfRange(fileData, 6, 10);
-        pictureOffset = bytesToInt(Arrays.copyOfRange(fileData, 10, 14));
-        infoHeaderLength = bytesToInt(Arrays.copyOfRange(fileData, 14, 18));
-        width = bytesToInt(Arrays.copyOfRange(fileData, 18, 22));
-        height = bytesToInt(Arrays.copyOfRange(fileData, 22, 26));
-        numPlanes = bytesToInt(Arrays.copyOfRange(fileData, 26, 28));
-        bitsPerPixel = bytesToInt(Arrays.copyOfRange(fileData, 28, 30));
-        compressionType = bytesToInt(Arrays.copyOfRange(fileData, 30, 34));
-        pictureSize = bytesToInt(Arrays.copyOfRange(fileData, 34, 38));
-        horizontalResolution = bytesToInt(Arrays.copyOfRange(fileData, 38, 42));
-        verticalResolution = bytesToInt(Arrays.copyOfRange(fileData, 42, 46));
-        numUsedColors = bytesToInt(Arrays.copyOfRange(fileData, 46, 50));
-        numImportantColors = bytesToInt(Arrays.copyOfRange(fileData, 50, 54));
-        extraHeaderBytes = Arrays.copyOfRange(fileData, 54, pictureOffset);
-        pictureData = Arrays.copyOfRange(fileData, pictureOffset, pictureOffset + pictureSize);
-        if(pictureOffset + pictureSize < fileSize) {
-            extraPictureBytes = Arrays.copyOfRange(fileData, pictureOffset + pictureSize, fileSize);
-        }
-    }
-
-    /**
-     * Writes the BMP to the same file this parser was initialized with.  Equivalent to {@code writeImage(file.getAbsolutePath())},
-     * where {@code file} is the File representation of the file this parser was initialized with.
-     */
-    public void writeImage() throws IOException {
-        writeImage(file.getAbsolutePath());
-    }
-
-    /**
-     * Writes the BMP to the specified output file.  Only modifies the reserved bytes section, everything else is left as-is.
-     *
-     * @param outputPath The output file path.
      * @throws IOException If an I/O error occurs.
      */
-    public void writeImage(String outputPath) throws IOException {
-        //Update reserved bytes info
-        int offset = 6;
-        for (int i = 0; i < reservedBytes.length; i++) {
-            fileData[i + offset] = reservedBytes[i];
-        }
-        Files.write(new File(outputPath).toPath(), fileData);
+    public void writeImage() throws IOException {
+        FileOutputStream fos = new FileOutputStream(this.file);
+        fos.write(id.getBytes());
+        fos.write(intToBytes(fileSize));
+        fos.write(reservedBytes);
+        fos.write(intToBytes(pictureOffset));
+        fos.write(intToBytes(infoHeaderLength));
+        fos.write(intToBytes(width));
+        fos.write(intToBytes(height));
+        fos.write(intToBytes(numPlanes));
+        fos.write(intToBytes(bitsPerPixel));
+        fos.write(intToBytes(compressionType));
+        fos.write(intToBytes(pictureSize));
+        fos.write(intToBytes(horizontalResolution));
+        fos.write(intToBytes(verticalResolution));
+        fos.write(intToBytes(numUsedColors));
+        fos.write(intToBytes(numImportantColors));
+        fos.write(extraHeaderBytes);
+        fos.write(pictureData);
+        fos.close();
     }
 
     public String getId() {
@@ -193,55 +145,38 @@ public class BmpWriter {
     }
 
     /**
-     * Convert a byte array to int.
+     * Convert an int to byte array. See {@link BmpParser#bytesToInt(byte[])} for original source.
      *
-     * @param bytes The bytes to convert to int.  {@code bytes.length} must be between 1 and 4.
-     * @return The converted int.
-     * @see <a href="https://stackoverflow.com/a/2383729/2333689">Source</a>
+     * @param number The int to convert to bytes.
+     * @return The converted byte array.
      */
-    private int bytesToInt(byte[] bytes) {
-        if (bytes.length > 4) {
-            throw new IllegalArgumentException("Can only convert up to 4 bytes to int");
-        } else if (bytes.length < 1) {
-            throw new IllegalArgumentException("Need at least 1 byte to convert");
-        }
-        byte[] parsedBytes = new byte[4];
-        for (int i = 0; i < bytes.length; i++) {
-            parsedBytes[i] = bytes[i];
-        }
-        ByteBuffer bb = ByteBuffer.wrap(parsedBytes);
+    private byte[] intToBytes(int number) {
+        ByteBuffer bb = ByteBuffer.allocate(4);
         bb.order(ByteOrder.LITTLE_ENDIAN);
-        return bb.getInt();
+        bb.putInt(number);
+        return bb.array();
     }
 
     public static class BmpWriterBuilder {
-        private File file;
-        private byte[] fileData;
+        private File outputFile;
         private String id = "BM";
-        private int fileSize;
         byte[] reservedBytes;
-        int pictureOffset;
-        private int infoHeaderLength;
-        private int width, height;
-        private int numPlanes;
-        private int bitsPerPixel;
-        private int compressionType;
-        private int pictureSize;
-        private int horizontalResolution;
-        private int verticalResolution;
-        private int numUsedColors;
-        private int numImportantColors;
+//        int pictureOffset;
+//        private int infoHeaderLength;
+        private int width;
+        private int height;
+        private int numPlanes = 1;
+        private int bitsPerPixel = 8;
+        private int compressionType = 0;
+        private int horizontalResolution = 0;
+        private int verticalResolution = 0;
+        private int numUsedColors = 256;
+        private int numImportantColors = 0;
         private byte[] pictureData;
-        private byte[] extraHeaderBytes;
-        private byte[] extraPictureBytes = new byte[0];
+        private byte[] extraHeaderBytes = new byte[] {0, 0, 0, 0, 1, 1, 1, 0, 2, 2, 2, 0, 3, 3, 3, 0, 4, 4, 4, 0, 5, 5, 5, 0, 6, 6, 6, 0, 7, 7, 7, 0, 8, 8, 8, 0, 9, 9, 9, 0, 10, 10, 10, 0, 11, 11, 11, 0, 12, 12, 12, 0, 13, 13, 13, 0, 14, 14, 14, 0, 15, 15, 15, 0, 16, 16, 16, 0, 17, 17, 17, 0, 18, 18, 18, 0, 19, 19, 19, 0, 20, 20, 20, 0, 21, 21, 21, 0, 22, 22, 22, 0, 23, 23, 23, 0, 24, 24, 24, 0, 25, 25, 25, 0, 26, 26, 26, 0, 27, 27, 27, 0, 28, 28, 28, 0, 29, 29, 29, 0, 30, 30, 30, 0, 31, 31, 31, 0, 32, 32, 32, 0, 33, 33, 33, 0, 34, 34, 34, 0, 35, 35, 35, 0, 36, 36, 36, 0, 37, 37, 37, 0, 38, 38, 38, 0, 39, 39, 39, 0, 40, 40, 40, 0, 41, 41, 41, 0, 42, 42, 42, 0, 43, 43, 43, 0, 44, 44, 44, 0, 45, 45, 45, 0, 46, 46, 46, 0, 47, 47, 47, 0, 48, 48, 48, 0, 49, 49, 49, 0, 50, 50, 50, 0, 51, 51, 51, 0, 52, 52, 52, 0, 53, 53, 53, 0, 54, 54, 54, 0, 55, 55, 55, 0, 56, 56, 56, 0, 57, 57, 57, 0, 58, 58, 58, 0, 59, 59, 59, 0, 60, 60, 60, 0, 61, 61, 61, 0, 62, 62, 62, 0, 63, 63, 63, 0, 64, 64, 64, 0, 65, 65, 65, 0, 66, 66, 66, 0, 67, 67, 67, 0, 68, 68, 68, 0, 69, 69, 69, 0, 70, 70, 70, 0, 71, 71, 71, 0, 72, 72, 72, 0, 73, 73, 73, 0, 74, 74, 74, 0, 75, 75, 75, 0, 76, 76, 76, 0, 77, 77, 77, 0, 78, 78, 78, 0, 79, 79, 79, 0, 80, 80, 80, 0, 81, 81, 81, 0, 82, 82, 82, 0, 83, 83, 83, 0, 84, 84, 84, 0, 85, 85, 85, 0, 86, 86, 86, 0, 87, 87, 87, 0, 88, 88, 88, 0, 89, 89, 89, 0, 90, 90, 90, 0, 91, 91, 91, 0, 92, 92, 92, 0, 93, 93, 93, 0, 94, 94, 94, 0, 95, 95, 95, 0, 96, 96, 96, 0, 97, 97, 97, 0, 98, 98, 98, 0, 99, 99, 99, 0, 100, 100, 100, 0, 101, 101, 101, 0, 102, 102, 102, 0, 103, 103, 103, 0, 104, 104, 104, 0, 105, 105, 105, 0, 106, 106, 106, 0, 107, 107, 107, 0, 108, 108, 108, 0, 109, 109, 109, 0, 110, 110, 110, 0, 111, 111, 111, 0, 112, 112, 112, 0, 113, 113, 113, 0, 114, 114, 114, 0, 115, 115, 115, 0, 116, 116, 116, 0, 117, 117, 117, 0, 118, 118, 118, 0, 119, 119, 119, 0, 120, 120, 120, 0, 121, 121, 121, 0, 122, 122, 122, 0, 123, 123, 123, 0, 124, 124, 124, 0, 125, 125, 125, 0, 126, 126, 126, 0, 127, 127, 127, 0, -128, -128, -128, 0, -127, -127, -127, 0, -126, -126, -126, 0, -125, -125, -125, 0, -124, -124, -124, 0, -123, -123, -123, 0, -122, -122, -122, 0, -121, -121, -121, 0, -120, -120, -120, 0, -119, -119, -119, 0, -118, -118, -118, 0, -117, -117, -117, 0, -116, -116, -116, 0, -115, -115, -115, 0, -114, -114, -114, 0, -113, -113, -113, 0, -112, -112, -112, 0, -111, -111, -111, 0, -110, -110, -110, 0, -109, -109, -109, 0, -108, -108, -108, 0, -107, -107, -107, 0, -106, -106, -106, 0, -105, -105, -105, 0, -104, -104, -104, 0, -103, -103, -103, 0, -102, -102, -102, 0, -101, -101, -101, 0, -100, -100, -100, 0, -99, -99, -99, 0, -98, -98, -98, 0, -97, -97, -97, 0, -96, -96, -96, 0, -95, -95, -95, 0, -94, -94, -94, 0, -93, -93, -93, 0, -92, -92, -92, 0, -91, -91, -91, 0, -90, -90, -90, 0, -89, -89, -89, 0, -88, -88, -88, 0, -87, -87, -87, 0, -86, -86, -86, 0, -85, -85, -85, 0, -84, -84, -84, 0, -83, -83, -83, 0, -82, -82, -82, 0, -81, -81, -81, 0, -80, -80, -80, 0, -79, -79, -79, 0, -78, -78, -78, 0, -77, -77, -77, 0, -76, -76, -76, 0, -75, -75, -75, 0, -74, -74, -74, 0, -73, -73, -73, 0, -72, -72, -72, 0, -71, -71, -71, 0, -70, -70, -70, 0, -69, -69, -69, 0, -68, -68, -68, 0, -67, -67, -67, 0, -66, -66, -66, 0, -65, -65, -65, 0, -64, -64, -64, 0, -63, -63, -63, 0, -62, -62, -62, 0, -61, -61, -61, 0, -60, -60, -60, 0, -59, -59, -59, 0, -58, -58, -58, 0, -57, -57, -57, 0, -56, -56, -56, 0, -55, -55, -55, 0, -54, -54, -54, 0, -53, -53, -53, 0, -52, -52, -52, 0, -51, -51, -51, 0, -50, -50, -50, 0, -49, -49, -49, 0, -48, -48, -48, 0, -47, -47, -47, 0, -46, -46, -46, 0, -45, -45, -45, 0, -44, -44, -44, 0, -43, -43, -43, 0, -42, -42, -42, 0, -41, -41, -41, 0, -40, -40, -40, 0, -39, -39, -39, 0, -38, -38, -38, 0, -37, -37, -37, 0, -36, -36, -36, 0, -35, -35, -35, 0, -34, -34, -34, 0, -33, -33, -33, 0, -32, -32, -32, 0, -31, -31, -31, 0, -30, -30, -30, 0, -29, -29, -29, 0, -28, -28, -28, 0, -27, -27, -27, 0, -26, -26, -26, 0, -25, -25, -25, 0, -24, -24, -24, 0, -23, -23, -23, 0, -22, -22, -22, 0, -21, -21, -21, 0, -20, -20, -20, 0, -19, -19, -19, 0, -18, -18, -18, 0, -17, -17, -17, 0, -16, -16, -16, 0, -15, -15, -15, 0, -14, -14, -14, 0, -13, -13, -13, 0, -12, -12, -12, 0, -11, -11, -11, 0, -10, -10, -10, 0, -9, -9, -9, 0, -8, -8, -8, 0, -7, -7, -7, 0, -6, -6, -6, 0, -5, -5, -5, 0, -4, -4, -4, 0, -3, -3, -3, 0, -2, -2, -2, 0, -1, -1, -1, 0};
 
         public BmpWriterBuilder file(File file) {
-            this.file = file;
-            return this;
-        }
-
-        public BmpWriterBuilder fileData(byte[] fileData) {
-            this.fileData = fileData;
+            this.outputFile = file;
             return this;
         }
 
@@ -250,23 +185,8 @@ public class BmpWriter {
             return this;
         }
 
-        public BmpWriterBuilder fileSize(int fileSize) {
-            this.fileSize = fileSize;
-            return this;
-        }
-
         public BmpWriterBuilder reservedBytes(byte[] reservedBytes) {
             this.reservedBytes = reservedBytes;
-            return this;
-        }
-
-        public BmpWriterBuilder pictureOffset(int pictureOffset) {
-            this.pictureOffset = pictureOffset;
-            return this;
-        }
-
-        public BmpWriterBuilder infoHeaderLength(int infoHeaderLength) {
-            this.infoHeaderLength = infoHeaderLength;
             return this;
         }
 
@@ -280,23 +200,8 @@ public class BmpWriter {
             return this;
         }
 
-        public BmpWriterBuilder numPlanes(int numPlanes) {
-            this.numPlanes = numPlanes;
-            return this;
-        }
-
-        public BmpWriterBuilder bitsPerPixel(int bitsPerPixel) {
-            this.bitsPerPixel = bitsPerPixel;
-            return this;
-        }
-
         public BmpWriterBuilder compressionType(int compressionType) {
             this.compressionType = compressionType;
-            return this;
-        }
-
-        public BmpWriterBuilder pictureSize(int pictureSize) {
-            this.pictureSize = pictureSize;
             return this;
         }
 
@@ -322,16 +227,6 @@ public class BmpWriter {
 
         public BmpWriterBuilder pictureData(byte[] pictureData) {
             this.pictureData = pictureData;
-            return this;
-        }
-
-        public BmpWriterBuilder extraHeaderBytes(byte[] extraHeaderBytes) {
-            this.extraHeaderBytes = extraHeaderBytes;
-            return this;
-        }
-
-        public BmpWriterBuilder extraPictureBytes(byte[] extraPictureBytes) {
-            this.extraPictureBytes = extraPictureBytes;
             return this;
         }
 
