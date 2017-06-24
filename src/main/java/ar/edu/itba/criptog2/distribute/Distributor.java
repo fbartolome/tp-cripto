cf56c562d1bf5fe7fdd42449bb61375548c9ce70
Juan Li Puma <juan.lipuma@restorando.com>
2017-06-24 09:01:40 -0300

    Improve BmpParser
    - Actually parse BMP
    - Only allow initialization with one image, will need multiple parsers to parse multiple files
    - Hardcode image path in Distributor for easier testing

--- src/main/java/ar/edu/itba/criptog2/distribute/Distributor.java
+++ src/main/java/ar/edu/itba/criptog2/distribute/Distributor.java
@@ -1,11 +1,10 @@
 package ar.edu.itba.criptog2.distribute;
 
-import java.io.IOException;
-import java.util.Scanner;
-
 import ar.edu.itba.criptog2.Worker;
 import ar.edu.itba.criptog2.util.BmpParser;
 import net.sourceforge.argparse4j.inf.Namespace;
+
+import java.io.IOException;
 
 public class Distributor implements Worker {
 	
@@ -16,15 +15,17 @@ public class Distributor implements Worker {
 	public static Distributor createFromNamespace(final Namespace ns) {
 		final Distributor distributor = new Distributor();
 		
-		Scanner s = new Scanner(System.in);
-		System.out.print("Enter bitmap image path: ");
-		String path = s.nextLine();
-		BmpParser parser = new BmpParser(path);
+//		Scanner s = new Scanner(System.in);
+//		System.out.print("Enter bitmap image path: ");
+//		String path = s.nextLine();
+        //TODO remove hardcoded string
+		String path = "img/Albertssd.bmp";
+
+		BmpParser parser = null;
 		try {
-			byte[][] data = parser.readAllFiles();
-			System.out.println(data);
+			parser = new BmpParser(path);
 		} catch (IOException e) {
-			System.err.println("Could not read all files: " + e.getMessage());
+			System.err.println("Could not read file: " + e.getMessage());
 			e.printStackTrace();
 		}
 		
--- src/main/java/ar/edu/itba/criptog2/util/BmpParser.java
+++ src/main/java/ar/edu/itba/criptog2/util/BmpParser.java
@@ -2,48 +2,192 @@ package ar.edu.itba.criptog2.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.file.Files;
+import java.util.Arrays;
 
 /**
- * Simple BMP parser. Can convert BMP images to byte arrays and back.
+ * Simple BMP parser. Can read and extract data and metadata from BMP file, and writeImage the file back.
  */
 public class BmpParser {
 
-    private final File[] files;
+    private final File file;
+    private byte[] fileData;
+    private String id;
+    private int fileSize;
+    byte[] reservedBytes;
+    int pictureOffset;
+    private int infoHeaderLength;
+    private int width, height;
+    private int numPlanes;
+    private int bitsPerPixel;
+    private int compressionType;
+    private int pictureSize;
+    private int horizontalResolution;
+    private int verticalResolution;
+    private int numUsedColors;
+    private int numImportantColors;
+    private byte[] pictureData;
+    private byte[] extraHeaderBytes;
+    private byte[] extraPictureBytes = new byte[0];
 
-    public BmpParser(String... bitmapImagePaths) {
-        files = new File[bitmapImagePaths.length];
-        for (int i = 0; i < files.length; i++) {
-            File f = new File(bitmapImagePaths[i]);
-            if(!f.exists()) {
-                throw new IllegalArgumentException("Image does not exist");
-            }
-            if(!f.canRead()) {
-                throw new IllegalArgumentException("Image is not readable");
-            }
-            files[i] = f;
+    public BmpParser(String bitmapImagePath) throws IOException {
+        if(bitmapImagePath == null || bitmapImagePath.isEmpty()) {
+            throw new IllegalArgumentException("Invalid bitmap image path provided");
+        }
+        file = new File(bitmapImagePath);
+        if (!file.exists()) {
+            throw new IllegalArgumentException("Image does not exist");
+        }
+        if (file.isDirectory()) {
+            throw new IllegalArgumentException("Provided path is a directory, not a file");
+        }
+        if (!file.canRead()) {
+            throw new IllegalArgumentException("Image is not readable");
+        }
+        parse();
+    }
+
+    /**
+     * Parses the BMP this parser was initialized with, reading all header data and image data.
+     *
+     * @throws IOException If an I/O error occurs reading any of the file.
+     * @see <a href="https://stackoverflow.com/a/10497780/2333689">Source for converting File to byte[]</a>
+     */
+    private void parse() throws IOException {
+        fileData = Files.readAllBytes(file.toPath());
+//        ByteArrayInputStream stream = new ByteArrayInputStream(fileData);
+//        ArrayList<Byte> buffer = new ArrayList<>();
+        id = new String(Arrays.copyOfRange(fileData, 0, 2));
+        fileSize = bytesToInt(Arrays.copyOfRange(fileData, 2, 6));
+        reservedBytes = Arrays.copyOfRange(fileData, 6, 10);
+        pictureOffset = bytesToInt(Arrays.copyOfRange(fileData, 10, 14));
+        infoHeaderLength = bytesToInt(Arrays.copyOfRange(fileData, 14, 18));
+        width = bytesToInt(Arrays.copyOfRange(fileData, 18, 22));
+        height = bytesToInt(Arrays.copyOfRange(fileData, 22, 26));
+        numPlanes = bytesToInt(Arrays.copyOfRange(fileData, 26, 28));
+        bitsPerPixel = bytesToInt(Arrays.copyOfRange(fileData, 28, 30));
+        compressionType = bytesToInt(Arrays.copyOfRange(fileData, 30, 34));
+        pictureSize = bytesToInt(Arrays.copyOfRange(fileData, 34, 38));
+        horizontalResolution = bytesToInt(Arrays.copyOfRange(fileData, 38, 42));
+        verticalResolution = bytesToInt(Arrays.copyOfRange(fileData, 42, 46));
+        numUsedColors = bytesToInt(Arrays.copyOfRange(fileData, 46, 50));
+        numImportantColors = bytesToInt(Arrays.copyOfRange(fileData, 50, 54));
+        extraHeaderBytes = Arrays.copyOfRange(fileData, 54, pictureOffset);
+        pictureData = Arrays.copyOfRange(fileData, pictureOffset, pictureOffset + pictureSize);
+        if(pictureOffset + pictureSize < fileSize) {
+            extraPictureBytes = Arrays.copyOfRange(fileData, pictureOffset + pictureSize, fileSize);
         }
     }
 
     /**
-     * Reads all the files this parser was initialized with, converting each file to a byte[].
-     *
-     * @return An array of byte arrays, where each entry corresponds to a single file.
-     * @throws IOException If an I/O error occurs reading any of the files.
-     * @see <a href="https://stackoverflow.com/a/10497780/2333689">Source</a>
+     * Writes the BMP to the same file this parser was initialized with.  Equivalent to {@code writeImage(file.getAbsolutePath())},
+     * where {@code file} is the File representation of the file this parser was initialized with.
      */
-    public byte[][] readAllFiles() throws IOException {
-        byte[][] result = new byte[files.length][];
-        for (int i = 0; i < files.length; i++) {
-            result[i] = Files.readAllBytes(files[i].toPath());
-        }
-        return result;
+    public void writeImage() throws IOException {
+        writeImage(file.getAbsolutePath());
     }
 
-    public void writeAllFiles(byte[][] fileBytes) throws IOException {
-        // TODO: Don't take arbitrary byte arrays as parameter, anybody can write anything (even different data) to the original files.
-        for (int i = 0; i < files.length; i++) {
-            Files.write(files[i].toPath(), fileBytes[i]);
+    /**
+     * Writes the BMP to the specified output file.  Only modifies the reserved bytes section, everything else is left as-is.
+     *
+     * @param outputPath The output file path.
+     * @throws IOException If an I/O error occurs.
+     */
+    public void writeImage(String outputPath) throws IOException {
+        //Update reserved bytes info
+        int offset = 6;
+        for (int i = 0; i < reservedBytes.length; i++) {
+            fileData[i + offset] = reservedBytes[i];
         }
+        Files.write(new File(outputPath).toPath(), fileData);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public byte[] getReservedBytes() {
+        return reservedBytes;
+    }
+
+    public int getPictureOffset() {
+        return pictureOffset;
+    }
+
+    public int getInfoHeaderLength() {
+        return infoHeaderLength;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getNumPlanes() {
+        return numPlanes;
+    }
+
+    public int getBitsPerPixel() {
+        return bitsPerPixel;
+    }
+
+    public int getCompressionType() {
+        return compressionType;
+    }
+
+    public int getPictureSize() {
+        return pictureSize;
+    }
+
+    public int getHorizontalResolution() {
+        return horizontalResolution;
+    }
+
+    public int getVerticalResolution() {
+        return verticalResolution;
+    }
+
+    public int getNumUsedColors() {
+        return numUsedColors;
+    }
+
+    public int getNumImportantColors() {
+        return numImportantColors;
+    }
+
+    @Override
+    public String toString() {
+        return "BmpParser, parsed " + width + "x" + height + " bitmap image from " + file.getPath();
+    }
+
+    /**
+     * Convert a byte array to int.
+     *
+     * @param bytes The bytes to convert to int.  {@code bytes.length} must be between 1 and 4.
+     * @return The converted int.
+     * @see <a href="https://stackoverflow.com/a/2383729/2333689">Source</a>
+     */
+    private int bytesToInt(byte[] bytes) {
+        if (bytes.length > 4) {
+            throw new IllegalArgumentException("Can only convert up to 4 bytes to int");
+        } else if (bytes.length < 1) {
+            throw new IllegalArgumentException("Need at least 1 byte to convert");
+        }
+        byte[] parsedBytes = new byte[4];
+        for (int i = 0; i < bytes.length; i++) {
+            parsedBytes[i] = bytes[i];
+        }
+        ByteBuffer bb = ByteBuffer.wrap(parsedBytes);
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        return bb.getInt();
     }
 }
