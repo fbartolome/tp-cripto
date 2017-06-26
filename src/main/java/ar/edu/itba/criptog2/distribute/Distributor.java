31410e14e1839c45514dd6fc0af6801b9f6a8e15
Juan Li Puma <juanlipuma94@gmail.com>
2017-06-26 17:10:48 -0300

    Simplify conditional and message

--- src/main/java/ar/edu/itba/criptog2/distribute/Distributor.java
+++ src/main/java/ar/edu/itba/criptog2/distribute/Distributor.java
@@ -81,9 +81,8 @@ public class Distributor implements Worker {
 				} else {
 					int secretPixelCount = distributor.secretPicture.getWidth() * distributor.secretPicture.getHeight();
 					int shadowPixelCount = shadow.getWidth() * shadow.getHeight();
-					int minPixelCount = secretPixelCount*8/distributor.k;
-					if(shadowPixelCount < minPixelCount) {
-						System.err.println("All shadows should have at least " + minPixelCount + " pixels, " + shadowFiles[i].getName() + " has " + shadowPixelCount);
+					if(secretPixelCount * (8/distributor.k) < shadowPixelCount) {
+						System.err.println("Incompatible secret/shadow dimensions.");
 						System.err.println("Aborting.");
 						System.exit(1);
 					}
